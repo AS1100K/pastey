3b1b63c404d807372c9dee96c2b986a7110b587f
Aditya Kumar <git@adityais.dev>
2025-10-27 22:13:31 +0530

    Fix CI (#19)

--- test.sh
+++ test.sh
@@ -28,6 +28,8 @@ cd pastey-test-suite
 if [[ "$1" == "1.56.0" ]]; then
     cargo update -p quote --precise 1.0.40
     cargo update -p glob --precise 0.3.2
+    cargo update -p serde_derive --precise 1.0.210
+    cargo update -p proc-macro2 --precise 1.0.101
 fi
 
 cargo test
