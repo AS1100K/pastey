ed80778777e9c5a5a9a68f80419a6f5f99cc5d21
Aditya Kumar <git@adityais.dev>
2026-07-01 14:40:26 +0530

    Refactor snake case transformation logic

--- src/segment.rs
+++ src/segment.rs
@@ -208,23 +208,14 @@ pub(crate) fn paste(segments: &[Segment]) -> Result<String> {
                     "upper" => {
                         evaluated.push(last.to_uppercase());
                     }
-                    "snake" => {
+                    "snake" | "snake_number" => {
                         let mut acc = String::new();
                         let mut prev = '_';
+                        let include_numbers = ident.to_string().as_str() == "snake_number";
                         for ch in last.chars() {
-                            if ch.is_uppercase() && prev != '_' {
-                                acc.push('_');
-                            }
-                            acc.push(ch);
-                            prev = ch;
-                        }
-                        evaluated.push(acc.to_lowercase());
-                    }
-                    "snake_number" => {
-                        let mut acc = String::new();
-                        let mut prev = '_';
-                        for ch in last.chars() {
-                            if (ch.is_uppercase() || ch.is_numeric()) && prev != '_' {
+                            let should_trigger_underscore =
+                                ch.is_uppercase() || (include_numbers && ch.is_numeric());
+                            if should_trigger_underscore && prev != '_' {
                                 acc.push('_');
                             }
                             acc.push(ch);
